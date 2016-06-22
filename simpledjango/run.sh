b26772f664d95807e3c5a569d5c7b700e64c2cd4
Luis Polanco <siulpolb@outlook.com>
2016-06-22 12:50:23 -0500

    changes to support django 1.9

--- simpledjango/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ -z "$VCAP_APP_PORT" ];
-then SERVER_PORT=5000;
-else SERVER_PORT="$VCAP_APP_PORT";
-fi
-echo port is------------------- $SERVER_PORT
-#!/bin/bash
-python manage.py syncdb --noinput
